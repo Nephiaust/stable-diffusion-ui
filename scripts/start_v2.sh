3ab1bc101b368572c31b205d99254b8a444b4714
NephiAust <29741794+Nephiaust@users.noreply.github.com>
2023-05-28 06:47:21 +0930

    Reset local git repo. * Start of new script * Added block markers * Changed detection method for OS and ARCH, to handle Linux/Mac on arm64 * Merged functions.sh * Merged bootstrap.sh * Added new parameter to allow for specifying installation dir * Added displayed comments on actions taken * Quietten installers to make startup easier to read

--- scripts/start_v2.sh
+++ scripts/start_v2.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-source ./scripts/functions.sh
+INSTALL_DIR="$(eval echo ~$USER)/stablediffusion"
+
+# *******************************************************
+# *******************************************************
+# **                                                   **
+# **            DO NOT EDIT BELOW THIS BLOCK           **
+# **                                                   **
+# **                INITIALISATION BLOCK               **
+# **                                                   **
+# *******************************************************
+# *******************************************************
 
 set -o pipefail
+
+echo "Checking & Installing requirements for Easy Diffusion"
+
+#source $INSTALL_DIR/scripts/functions.sh
+
+# *******************************************************
+# *******************************************************
+# **                                                   **
+# **              INTERNAL VARIABLES BLOCK             **
+# **                                                   **
+# *******************************************************
+# *******************************************************
 
 OS_NAME=$(uname -s)
 case "${OS_NAME}" in
@@ -13,31 +35,87 @@ esac
 
 OS_ARCH=$(uname -m)
 case "${OS_ARCH}" in
-    x86_64*)    OS_ARCH="64";;
-    arm64*)     OS_ARCH="aarch64";;
-    aarch64*)     OS_ARCH="aarch64";;
-    *)          echo "Unknown system architecture: $OS_ARCH! This script runs only on x86_64 or arm64" && exit
+    x86_64*)
+        OS_ARCH="64";;
+    arm64*)     
+        case "${OS_NAME}" in
+            Linux*)     OS_ARCH="aarch64";;
+            *)          OS_ARCH="arm64";;
+        esac;;
+    aarch64*)
+        OS_ARCH="aarch64";;
+    *)
+        echo "Unknown system architecture: $OS_ARCH! This script runs only on x86_64 or arm64" && exit
 esac
+
+export MAMBA_ROOT_PREFIX="$INSTALL_DIR/installer_files/mamba"
+INSTALL_ENV_DIR="$INSTALL_DIR/installer_files/env"
+LEGACY_INSTALL_ENV_DIR="$INSTALL_DIR/installer"
+MICROMAMBA_DOWNLOAD_URL="https://micro.mamba.pm/api/micromamba/${OS_NAME}-${OS_ARCH}/latest"
+umamba_exists="F"
+
+# *******************************************************
+# *******************************************************
+# **                                                   **
+# **                 FUNCTIONS BLOCK                   **
+# **                                                   **
+# *******************************************************
+# *******************************************************
+
+fail() {
+    echo
+    echo "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
+    echo
+    if [ "$1" != "" ]; then
+        echo ERROR: $1
+    else
+        echo An error occurred.
+    fi
+    cat <<EOF
+
+Error downloading Stable Diffusion UI. Sorry about that, please try to:
+ 1. Run this installer again.
+ 2. If that doesn't fix it, please try the common troubleshooting steps at https://github.com/cmdr2/stable-diffusion-ui/wiki/Troubleshooting
+ 3. If those steps don't help, please copy *all* the error messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB
+ 4. If that doesn't solve the problem, please file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues
+
+Thanks!
+
+
+EOF
+    read -p "Press any key to continue"
+    exit 1
+
+}
+
+filesize() {
+    case "$(uname -s)" in
+        Linux*)     stat -c "%s" $1;;
+        Darwin*)    /usr/bin/stat -f "%z" $1;;
+        *)          echo "Unknown OS: $OS_NAME! This script runs only on Linux or Mac" && exit
+    esac
+}
+
+
+# *******************************************************
+# *******************************************************
+# **                                                   **
+# **                  MAIN CODE BLOCK                  **
+# **                                                   **
+# *******************************************************
+# *******************************************************
 
 if ! which curl &> /dev/null; then fail "'curl' not found. Please install curl."; fi
 if ! which tar &> /dev/null; then fail "'tar' not found. Please install tar."; fi
 if ! which bzip2 &> /dev/null; then fail "'bzip2' not found. Please install bzip2."; fi
-if ! which git &> /dev/null; then fail "'git' not found. Please install git."; fi
+#if ! which git &> /dev/null; then fail "'git' not found. Please install git."; fi
 
 if pwd | grep ' '
     then
         fail "The installation directory's path contains a space character. Conda will fail to install. Please change the directory."
 fi
 
-# https://mamba.readthedocs.io/en/latest/installation.html
-if [ "$OS_NAME" == "linux" ] && [ "$OS_ARCH" == "arm64" ]; then OS_ARCH="aarch64"; fi
 
-# config
-export MAMBA_ROOT_PREFIX="$(pwd)/installer_files/mamba"
-INSTALL_ENV_DIR="$(pwd)/installer_files/env"
-LEGACY_INSTALL_ENV_DIR="$(pwd)/installer"
-MICROMAMBA_DOWNLOAD_URL="https://micro.mamba.pm/api/micromamba/${OS_NAME}-${OS_ARCH}/latest"
-umamba_exists="F"
 
 if [ -e "$INSTALL_ENV_DIR" ]; then export PATH="$INSTALL_ENV_DIR/bin:$PATH"; fi
 
@@ -46,14 +124,86 @@ PACKAGES_TO_INSTALL=""
 if [ ! -e "$LEGACY_INSTALL_ENV_DIR/etc/profile.d/conda.sh" ] && [ ! -e "$INSTALL_ENV_DIR/etc/profile.d/conda.sh" ]
     then
         PACKAGES_TO_INSTALL="$PACKAGES_TO_INSTALL conda python=3.8.5"
+        echo " * Need to install python"
 fi
 
 if ! hash "git" &>/dev/null
     then
         PACKAGES_TO_INSTALL="$PACKAGES_TO_INSTALL git"
+        echo " * Need to install git"
 fi
 
-if "$MAMBA_ROOT_PREFIX/micromamba" --version &>/dev/null; then umamba_exists="T"; fi
+if "$MAMBA_ROOT_PREFIX/micromamba" --version &>/dev/null
+    then
+        umamba_exists="T"
+        echo " * Found micromamba"
+fi
 
-echo "Installation packages"
-echo $PACKAGES_TO_INSTALL
+# (if necessary) install git and conda into a contained environment
+if [ "$PACKAGES_TO_INSTALL" != "" ]; then
+    # download micromamba
+    if [ "$umamba_exists" == "F" ]; then
+        echo " * Downloading micromamba for $OS_ARCH on $OS_NAME to $MAMBA_ROOT_PREFIX/micromamba"
+
+        mkdir -p "$MAMBA_ROOT_PREFIX"
+        curl --progress-bar -L "$MICROMAMBA_DOWNLOAD_URL" | tar -xj -O bin/micromamba > "$MAMBA_ROOT_PREFIX/micromamba"
+
+        if [ "$?" != "0" ]; then
+            echo
+            echo "EE micromamba download failed"
+            echo "EE If the lines above contain 'bzip2: Cannot exec', your system doesn't have bzip2 installed"
+            echo "EE If there are network errors, please check your internet setup"
+            fail "micromamba download failed"
+        fi
+        
+        echo " * Enabling micromamba"
+        chmod u+x "$MAMBA_ROOT_PREFIX/micromamba" &> /dev/null
+
+        # test the mamba binary
+        echo " * Micromamba version"
+        "$MAMBA_ROOT_PREFIX/micromamba" --version
+    fi
+
+    # create the installer env
+    if [ ! -e "$INSTALL_ENV_DIR" ]; then
+        echo " * Creating empty micromamba envrionment at $INSTALL_ENV_DIR"
+        "$MAMBA_ROOT_PREFIX/micromamba" create -y -q --prefix "$INSTALL_ENV_DIR" || fail "unable to create the install environment"
+    fi
+
+    # Double check that the enviroment directory was created.
+    if [ ! -e "$INSTALL_ENV_DIR" ]; then
+        fail "There was a problem while installing $PACKAGES_TO_INSTALL using micromamba. Cannot continue."
+    fi
+
+    echo " * Packages to install:$PACKAGES_TO_INSTALL"
+    "$MAMBA_ROOT_PREFIX/micromamba" install -y --prefix "$INSTALL_ENV_DIR" -c conda-forge $PACKAGES_TO_INSTALL
+    if [ "$?" != "0" ]; then
+        fail "Installation of the packages '$PACKAGES_TO_INSTALL' failed."
+    fi
+fi
+
+# Verify that the environment exists
+if [ -e $INSTALL_ENV_DIR ]
+    then
+        echo " * Runtime environment set up and ready"
+        export PATH="$(INSTALL_ENV_DIR)/bin:$PATH"
+    else
+        fail "Missing runtime environment"
+fi
+
+if ! which git &> /dev/null; then fail "'git' not found. Please install git."; fi
+if ! which conda &> /dev/null; then fail "'conda' not found. Please install conda."; fi
+
+echo ""
+echo "Preparing to start Easy Diffusion"
+echo ""
+
+if [ -f "$(INSTALL_DIR)/scripts/config.sh" ]; then
+    echo " * Found server configuration settings"
+    source "$(INSTALL_DIR)/scripts/config.sh"
+fi
+
+if [ -f "$(INSTALL_DIR)/scripts/user_config.sh" ]; then
+    echo " * Found user configuration settings"
+    source "$(INSTALL_DIR)/scripts/user_config.sh"
+fi
