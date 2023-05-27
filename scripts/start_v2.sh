dad4a3472ea62283ade24db9798f083c48c79525
NephiAust <29741794+Nephiaust@users.noreply.github.com>
2023-05-28 07:25:25 +0930

    Completed setup script

--- scripts/start_v2.sh
+++ scripts/start_v2.sh
@@ -26,6 +26,8 @@ echo "Checking & Installing requirements for Easy Diffusion"
 # *******************************************************
 # *******************************************************
 
+ORIGINAL_DIR=$pwd
+
 OS_NAME=$(uname -s)
 case "${OS_NAME}" in
     Linux*)     OS_NAME="linux";;
@@ -198,12 +200,43 @@ echo ""
 echo "Preparing to start Easy Diffusion"
 echo ""
 
-if [ -f "$(INSTALL_DIR)/scripts/config.sh" ]; then
-    echo " * Found server configuration settings"
-    source "$(INSTALL_DIR)/scripts/config.sh"
+cd $INSTALL_DIR
+
+export PYTHONPATH=$(INSTALL_DIR)/installer_files/env/lib/python3.8/site-packages:$(INSTALL_DIR)/stable-diffusion/env/lib/python3.8/site-packages
+
+
+if [ -f "scripts/get_config.py" ]; then
+   export update_branch="$( python scripts/get_config.py --default=main update_branch )"
 fi
 
-if [ -f "$(INSTALL_DIR)/scripts/user_config.sh" ]; then
-    echo " * Found user configuration settings"
-    source "$(INSTALL_DIR)/scripts/user_config.sh"
+if [ "$update_branch" == "" ]; then
+    export update_branch="main"
 fi
+
+if [ -f "scripts/install_status.txt" ] && [ `grep -c sd_ui_git_cloned scripts/install_status.txt` -gt "0" ]; then
+    echo "Easy Diffusion's git repository was already installed. Updating from $update_branch.."
+
+    cd sd-ui-files
+
+    git reset --hard
+    git -c advice.detachedHead=false checkout "$update_branch"
+    git pull
+
+    cd ..
+else
+    printf "\n\nDownloading Easy Diffusion..\n\n"
+    printf "Using the $update_branch channel\n\n"
+
+    if git clone -b "$update_branch" https://github.com/cmdr2/stable-diffusion-ui.git sd-ui-files ; then
+        echo sd_ui_git_cloned >> scripts/install_status.txt
+    else
+        fail "git clone failed"
+    fi
+fi
+
+rm -rf ui
+cp -Rf sd-ui-files/ui .
+cp sd-ui-files/scripts/check_modules.py scripts/
+cp sd-ui-files/scripts/check_models.py scripts/
+cp sd-ui-files/scripts/get_config.py scripts/
+cp sd-ui-files/scripts/developer_console.sh .
